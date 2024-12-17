d0a34fb3d77cd66f45e2a7708d05f884604777d5
tony-guan-cru <tony.guan@cru.org>
2024-12-16 19:20:15 -0500

    Update main.tf to add dbt-triggers

--- poc-terraform/main.tf
+++ poc-terraform/main.tf
@@ -1,10 +1,10 @@
-module "fivetran-triggers" {
-  # TODO: use this when published; git@github.com:CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref={{version-tag}}
-  source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.13.0"
+module "fivetran_trigger" {
+  source = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
+
   name        = "fivetran-trigger"
   description = "A set of triggers to kick off Fivetran connection syncs for various systems"
-  secrets     = ["API_KEY", "API_SECRET"]
-  time_zone   = "UTC"
+
+  time_zone = "UTC"
   schedule = {
     el_fivetran_logs : {
       cron : "0 0 1 1 *",
@@ -13,6 +13,9 @@ module "fivetran-triggers" {
       }
     }
   }
+
+  secrets = ["API_KEY", "API_SECRET"]
+
   secret_managers = [
     "user:luis.rodriguez@cru.org",
     "user:matt.drees@cru.org",
@@ -21,5 +24,37 @@ module "fivetran-triggers" {
   project_id = local.project_id
 }
 
+module "dbt-triggers" {
+  source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
+  name        = "dbt-trigger"
+  description = "A set of triggers to kick off dbt jobs"
+  time_zone   = "UTC"
+  schedule = {
+    utilities : {
+      cron : "0 0 1 1 *",
+      argument = {
+        "job_id" = "23366"
+      }
+    }
+  }
 
+  secrets = ["DBT_TOKEN"]
 
+  secret_managers = [
+    "user:luis.rodriguez@cru.org",
+    "user:matt.drees@cru.org",
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
+    "group:dps-gcp-role-data-engineers@cru.org",
+  ]
+  project_id = local.project_id
+}
