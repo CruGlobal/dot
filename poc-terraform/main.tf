2ac14e6a497dce1d3d83aac912210c319f06bc05
tony-guan-cru <tony.guan@cru.org>
2024-12-30 08:47:49 -0500

    Add dockerfile and make changes for cloud run job

--- poc-terraform/main.tf
+++ poc-terraform/main.tf
@@ -3,7 +3,6 @@ module "fivetran-triggers" {
   source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.13.0"
   name        = "fivetran-trigger"
   description = "A set of triggers to kick off Fivetran connection syncs for various systems"
-  secrets     = ["API_KEY", "API_SECRET"]
   time_zone   = "UTC"
   schedule = {
     el_fivetran_logs : {
@@ -15,6 +14,30 @@ module "fivetran-triggers" {
   }
 
   secrets = ["API_KEY", "API_SECRET"]
+
+  secret_managers = [
+    "user:luis.rodriguez@cru.org",
+    "user:matt.drees@cru.org",
+    "group:dps-gcp-role-data-engineers@cru.org",
+  ]
+  project_id = local.project_id
+}
+
+module "dbt-triggers" {
+  source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
+  name        = "dbt-trigger"
+  description = "A set of triggers to kick off dbt jobs"
+  time_zone   = "UTC"
+  schedule = {
+    utilities : {
+      cron : "0 0 1 1 *",
+      argument = {
+        "job_id" = "23366"
+      }
+    }
+  }
+
+  secrets = ["DBT_TOKEN"]
 
   secret_managers = [
     "user:luis.rodriguez@cru.org",
