f645770c84c3569a42f71fa44347adeed7a6fdb8
tony-guan-cru <125295537+tony-guan-cru@users.noreply.github.com>
2025-01-07 09:30:20 -0500

    Add geography-process cloud run job (#28)
    * Geography process draft
    
    * Initial working draft, excluding the breakdown of functions
    
    * Break down large functions and add schedulers
    
    * Change to use the default service account and remove unused client methods
    
    * Rename function and add memory monitoring
    
    * Add dockerfile and make changes for cloud run job
    
    * Replace bigquery project and dataset with env var
    
    * Add unit tests

--- poc-terraform/main.tf
+++ poc-terraform/main.tf
@@ -1,10 +1,10 @@
 module "fivetran_trigger" {
+  # TODO: use this when published; git@github.com:CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref={{version-tag}}
   source = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
 
   name        = "fivetran-trigger"
   description = "A set of triggers to kick off Fivetran connection syncs for various systems"
-
-  time_zone = "UTC"
+  time_zone   = "UTC"
   schedule = {
     el_fivetran_logs : {
       cron : "0 0 1 1 *",
@@ -13,6 +13,33 @@ module "fivetran_trigger" {
       }
     }
   }
+
+  secrets = ["API_KEY", "API_SECRET"]
+
+  secret_managers = [
+    "user:luis.rodriguez@cru.org",
+    "user:matt.drees@cru.org",
+    "group:dps-gcp-role-data-engineers@cru.org",
+  ]
+  project_id = local.project_id
+}
+
+module "process-geography" {
+  source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
+  name        = "process-geography"
+  description = "Google Cloud Function to process geography data"
+  time_zone   = "UTC"
+  schedule = {
+    monthly-scheduler : {
+      cron : "0 2 10 * *",
+      argument = {
+        type = "monthly"
+      }
+    }
+  }
+
+  secrets = ["GEONAMES_USERNAME", "GEONAMES_PASSWORD", "MAXMIND_LICENSE_KEY"]
+
 
   secrets = ["API_KEY", "API_SECRET"]
 
