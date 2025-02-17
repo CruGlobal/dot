46a1c589b505b34e3af2e242cc6c6a2f71b01c0e
tony-guan-cru <tony.guan@cru.org>
2025-02-17 18:40:24 -0500

    Update main.tf

--- poc-terraform/main.tf
+++ poc-terraform/main.tf
@@ -24,6 +24,30 @@ module "fivetran_trigger" {
   project_id = local.project_id
 }
 
+module "process-geography" {
+  source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
+  name        = "process-geography"
+  description = "Google Cloud Function to process geography data"
+  time_zone   = "UTC"
+  schedule = {
+    monthly-scheduler : {
+      cron : "0 2 10 * *",
+      argument = {
+        type = "monthly"
+      }
+    }
+  }
+
+  secrets = ["GEONAMES_USERNAME", "GEONAMES_PASSWORD", "MAXMIND_LICENSE_KEY"]
+
+  secret_managers = [
+    "user:luis.rodriguez@cru.org",
+    "user:matt.drees@cru.org",
+    "group:dps-gcp-role-data-engineers@cru.org",
+  ]
+  project_id = local.project_id
+}
+
 
 module "dbt-triggers" {
   source      = "git::https://github.com/CruGlobal/cru-terraform-modules.git//gcp/cloudrun-function/scheduled-tasks?ref=v30.14.4"
