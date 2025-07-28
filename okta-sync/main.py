9b223d9daabed9d79aae99ec6094eb8d9846d195
tony-guan-cru <tony.guan@cru.org>
2025-07-28 12:57:11 -0400

    Good for production

--- okta-sync/main.py
+++ okta-sync/main.py
@@ -250,19 +250,19 @@ def get_data(
         data = r.json()
         links = r.links
         df = pd.DataFrame(data)
-        # while "next" in links:  # comment out this while loop for testing
-        #     page_count += 1
-        #     url = links["next"]["url"]
-        #     logger.info(f"Downloading {endpoint} on page {page_count} from {url}")
-        #     params = {None: None}
-        #     r = get_request(url, headers, params)
-        #     if isinstance(r, requests.Response):
-        #         data_next = r.json()
-        #         links = r.links
-        #         df_next = pd.DataFrame(data_next)
-        #         df = pd.concat([df, df_next], axis=0, ignore_index=True)
-        #     else:
-        #         break
+        while "next" in links:  # comment out this while loop for testing
+            page_count += 1
+            url = links["next"]["url"]
+            logger.info(f"Downloading {endpoint} on page {page_count} from {url}")
+            params = {None: None}
+            r = get_request(url, headers, params)
+            if isinstance(r, requests.Response):
+                data_next = r.json()
+                links = r.links
+                df_next = pd.DataFrame(data_next)
+                df = pd.concat([df, df_next], axis=0, ignore_index=True)
+            else:
+                break
         logger.info(f"All {endpoint} downloaded successfully.")
         return df
     else:
@@ -724,7 +724,7 @@ def sync_all_users(endpoint: str) -> None:
         "Content-Type": "application/json",
     }
     params = {"limit": "1000"} if endpoint == "group_members" else {"limit": "500"}
-    ids = ids[:2]  # for testing
+    # ids = ids[:2]  # for testing
     df = get_all_users(f"{endpoint.split('_')[0]}s", headers, params, ids, columns)
     df = match_schema(df, schema_json)
     df = df.drop_duplicates()
@@ -788,8 +788,7 @@ def trigger_sync():
 
         # Trigger dbt job via Pub/Sub message
         publish_pubsub_message(
-            # {"job_id": "85521"},
-            {"job_id": "23366"},
+            {"job_id": "85521"},
             "cloud-run-job-completed",
         )
         log_memory_usage("- After Pub/Sub Message")
