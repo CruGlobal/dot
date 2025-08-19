a1974f91e662e0f7ccb78f85ccf415a6e1812a1f
cklineFL <chad.kline@cru.org>
2025-08-19 11:33:11 -0600

    again

--- woo-sync/main.py
+++ woo-sync/main.py
@@ -15,7 +15,7 @@ from google.cloud import bigquery
 from decimal import Decimal, getcontext
 from collections import Counter
 
-project_name = os.environ.get("BIGQUERY_PROJECT_NAME", None)
+project_name = os.environ.get("BIGQUERY_EXECUTION_PROJECT_NAME", None)
 dataset_name = os.environ.get("BIGQUERY_DATASET_NAME", None)
 client = BigQueryClient(project=project_name)
 
@@ -273,7 +273,7 @@ def get_last_load_date_time(obj):
     """
     logger = logging.getLogger("primary_logger")
     query = obj
-    client = bigquery.Client(project=project_name)
+    client = bigquery.Client(project='cru-data-orchestration-poc')
     
     logger.info(f"project: {project_name}")
 
